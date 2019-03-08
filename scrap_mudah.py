18c21c3a80f69a0586f5a77d466d53c14dbfaa1e
Izzat Zainol <izzatz13@gmail.com>
2019-03-09 02:22:40 +0800

    Added html files
    Will be used in offline mode for faster scraping

--- scrap_mudah.py
+++ scrap_mudah.py
@@ -8,12 +8,14 @@ import codecs
 # url to search macbook in penang under category computer
 url = "https://www.mudah.my/Penang/Computers-and-Accessories-3060/macbook-for-sale?lst=0&fs=1&w=103&cg=3060&q=macbook&so=1&st=s"
 
+
 # proxy settings
 # proxies = {'http':'http://proxy.kssm.intel.com:911', 'https':'http://proxy.kssm.intel.com:911'}
 # r = requests.get(url, headers=headers, proxies = proxies)
 
 # driver = webdriver.Chrome(chromedriver_path)  # Optional argument, if not specified will search path.
 # driver.get(url)
+
 
 page = requests.get(url)
 
@@ -23,5 +25,6 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 print(soup.prettify())
 
-with open("out.log", "w") as file:
-    file.write(str(soup))
+# with open("out.log", "w") as file:
+#    file.write(str(soup))
+
