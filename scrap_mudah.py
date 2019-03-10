9639354d885ff0abdae724b1cdacbafe1b6a1896
Izzat Zainol <izzatz13@gmail.com>
2019-03-10 22:12:34 +0800

    Tidy up code to be in a function

--- scrap_mudah.py
+++ scrap_mudah.py
@@ -10,27 +10,28 @@ import codecs
 url = "https://www.mudah.my/Penang/Computers-and-Accessories-3060/macbook-for-sale?lst=0&fs=1&w=103&cg=3060&q=macbook&so=1&st=s"
 # url = "Used and New Laptops for sale, Buy second hand laptops in Malaysia, Sell laptops, computers, projectors, printers and hard disk drives in Malaysia.htm"
 
-# proxy settings
-# proxies = {'http':'http://proxy.kssm.intel.com:911', 'https':'http://proxy.kssm.intel.com:911'}
 # r = requests.get(url, headers=headers, proxies = proxies)
 
 # driver = webdriver.Chrome(chromedriver_path)  # Optional argument, if not specified will search path.
 # driver.get(url)
 
 
-page = requests.get(url)
+def extract_title():
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    result = soup.find_all(lambda tag: tag.name == 'h2' and tag.get('class') == ['list_title'])
 
-soup = BeautifulSoup(page.content, 'html.parser')
-
-result = soup.find_all(lambda tag: tag.name == 'h2' and tag.get('class') == ['list_title'])
-# result = soup.find_all(lambda tag: tag.name == 'div' and tag.get('class') == ['ads_price'])
-
-#print(result[0])
-
-for item in result:
-    print (item)
+    for item in result:
+        print (item)
 
 
+def main():
+    extract_title()
+
+
+if __name__ == "__main__":
+    # stuff only to run when not called via 'import' here
+	main()
 
 
 
